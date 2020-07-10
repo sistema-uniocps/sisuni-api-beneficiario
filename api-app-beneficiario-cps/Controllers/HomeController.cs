8fbf7dff19d133d6fa81c217d7e99222a05466e9
Robson Simeoni <robson.simeoni@gmail.com>
2020-07-10 14:20:21 -0300

    Inclusão da tela HOME na API do Beneficiário

---
+++ api-app-beneficiario-cps/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace api_app_beneficiario_cps.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Default";
+
+            return View();
+        }
+    }
+}
